d4bacd631dd93ee96a648644c72c9be4e83f7616
Victor Miroshnikov <me@vmiroshnikov.com>
2015-06-15 21:57:47 +0200

    add json examples

--- Main.hs
+++ Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import           Control.Applicative                  ((<$>))
-import           Controllers.Home                     (home, login)
+import           Controllers.Home                     (home, login, post)
 import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import           Network.Wai.Middleware.Static        (addBase, noDots,
                                                        staticPolicy, (>->))
@@ -14,6 +14,6 @@ main = do
   scotty port $ do
          middleware $ staticPolicy (noDots >-> addBase "static/images") -- for favicon.ico
          middleware logStdoutDev
-         home >> login
+         home >> login >> post
 
 
